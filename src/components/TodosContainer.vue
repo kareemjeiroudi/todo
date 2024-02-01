a2ec168a2b0560944ca0764adc5621f6bb6f4632
Kareem Jeiroudi <KareemJeiroudi@Gmail.com>
2024-02-01 17:24:23 +0100

    Add form to submit new todos

--- src/components/TodosContainer.vue
+++ src/components/TodosContainer.vue
@@ -1,14 +1,22 @@
 <template>
-  <div id="actions">
-    <button class="action-button">+</button>
-    <button class="action-button">-</button>
-  </div>
-  <div id="todos-container">
-    <div v-for="todo in todos" :key="todo.title" class="todo-item" :class="{ selected: todo.selected }">
-      <input type="checkbox" :value="todo.selected" @change="todo.selected = !todo.selected">
-      <TodoItem :title="todo.title" :done="todo.done"/>
+  <form @submit="deleteSelected" @submit.prevent>
+    <div id="actions">
+      <button class="action-button" type="button" @click="showAddForm = true">+</button>
+      <button class="action-button" type="submit">-</button>
     </div>
-  </div>
+
+    <div id="todos-container">
+      <form v-if="showAddForm" class="todo-item" @submit="addNewTodo" @submit.prevent>
+        <input id="new-todo-input" type="text" v-model="newTodoTitle" placeholder="Finish task RKU-314" class="green">
+        <input type="submit" hidden />
+      </form>
+
+      <div v-for="todo in todos" :key="todo.title" class="todo-item" :class="{ selected: todo.selected }">
+        <input type="checkbox" v-model="todo.selected">
+        <TodoItem :title="todo.title" :done="todo.done"/>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script setup lang="ts">
@@ -16,11 +24,42 @@
 import TodoItem from "@/components/TodoItem.vue";
 import { ref } from "vue";
 
+type Todo = {
+  id: number,
+  title: string,
+  selected: boolean,
+  done: boolean
+}
+
 const todos =  ref([
   { id: 1, title: "Todo item 01", selected: false, done: false},
   { id: 2, title: "Todo item 02", selected: false, done: false},
   { id: 3, title: "Todo item 03", selected: false, done: false},
+  { id: 4, title: "Todo item 04", selected: true, done: false},
+  { id: 5, title: "Todo item 05", selected: false, done: false},
 ])
+
+let newTodoTitle = '';
+const showAddForm = ref(false);
+
+function deleteSelected(): void {
+  todos.value = todos.value.filter(todo => !todo.selected);
+}
+
+function addNewTodo(): void {
+  if (!newTodoTitle.length) {
+    return;
+  }
+
+  const newTodo: Todo = {id: todos.value.length, title: newTodoTitle, selected: false, done: false };
+  todos.value = [newTodo, ...todos.value];
+  resetForm();
+}
+
+function resetForm(): void {
+  showAddForm.value = false;
+  newTodoTitle = '';
+}
 </script>
 
 <style scoped>
@@ -51,7 +90,18 @@ const todos =  ref([
   column-gap: 1em;
 }
 
+form.todo-item {
+  border: none;
+}
+
 .todo-item.selected {
   background-color: var(--color-text-accent);
+}
+
+#new-todo-input {
+  padding: .4em .8em;
+  font-size: 1.2rem;
+  width: 100%;
+  background-color: var(--color-background-soft);
 }
 </style>
